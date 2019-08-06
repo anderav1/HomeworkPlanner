5c89fc14fb16c64595df642d4cded92f56170595
Lexi Anderson <alexis.v.anderson@vanderbilt.edu>
2019-08-06 16:17:48 -0500

    Added delete & add functionality to course list

---
+++ HomeworkPlanner/Course List/CourseListModel.swift
@@ -0,0 +1,39 @@
+import UIKit
+
+protocol CourseListModelDelegate: class {
+    func dataChanged()
+}
+
+final class CourseListModel {
+    private(set) var courseList: [String]
+    
+    private weak var delegate: CourseListModelDelegate?
+    private let persistence: HomeworkTaskPersistence
+    
+    init(delegate: CourseListModelDelegate, persistence: HomeworkTaskPersistence) {
+        self.delegate = delegate
+        self.persistence = persistence
+        
+        courseList = persistence.courses.sorted(by: { $0 < $1 })
+    }
+}
+
+// Functions for editing the course list
+extension CourseListModel {
+    func add(course: String) {
+        persistence.add(course: course)
+        courseList.append(course)
+        delegate?.dataChanged()
+    }
+    
+//    func delete(course: String, at index: Int) {
+//        persistence.delete(course: course)
+//        courseList.remove(at: index)
+//        delegate?.dataChanged()
+//    }
+    
+    func delete(at index: Int) {
+        persistence.delete(course: courseList[index])
+        courseList.remove(at: index)
+    }
+}
--- HomeworkPlanner/Course List/CourseListViewController.swift
+++ HomeworkPlanner/Course List/CourseListViewController.swift
@@ -1,7 +1,8 @@
 import UIKit
 
 final class CourseListViewController: UIViewController {
-    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addCourseTextField: UITextField!
     
     private var model: CourseListModel!
 }
@@ -16,26 +17,45 @@ extension CourseListViewController {
     }
 }
 
-final class CourseListModel {
-    private(set) var courseList: [String]
-    
-    private weak var delegate: CourseListModelDelegate?
-    private let persistence: HomeworkTaskPersistence
-    
-    init(delegate: CourseListModelDelegate, persistence: HomeworkTaskPersistence) {
-        self.delegate = delegate
-        self.persistence = persistence
-        
-        courseList = persistence.courses.sorted(by: { $0 < $1 })
+extension CourseListViewController: CourseListModelDelegate {
+    func dataChanged() {
+        tableView.reloadData()
     }
 }
 
-extension CourseListModel {
-    private func add(course: String) {
-        persistence.add(course: course)
+extension CourseListViewController: UITableViewDelegate {
+    
+}
+
+extension CourseListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.courseList.count
     }
     
-    private func delete(course: String) {
-        persistence.delete(course: course)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "courseCell", for: indexPath)
+        
+        // set up the cell
+        cell.textLabel?.text = model.courseList[indexPath.row]
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            model.delete(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .right)
+        }
     }
 }
+
+extension CourseListViewController {
+    @IBAction func addCourse(_ sender: UITextField) {
+        if let newCourse = sender.text, !newCourse.isEmpty {
+            model.add(course: newCourse)
+        }
+    }
+    
+    
+}
+
